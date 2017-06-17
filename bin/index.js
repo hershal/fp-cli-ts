d135c49f484c6e62e114654844d85eb1a9a1205e
Hershal Bhave <hershal.bhave@gmail.com>
2017-06-17 17:39:51 -0500

    Add debug information to index

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
+const debug = require('debug')('Index');
+const verbose = require('debug')('Index:Verbose');
+
 const path = require('path');
 
 /* Node v8.1.2 does not support ES2015-style modules without --harmony. */
@@ -9,20 +12,33 @@ const Dispatcher = require('../dist/Dispatcher.js').default;
 
 let program = path.basename(process.argv[1], '.js');
 
+debug('Starting up');
+
 if (program == 'index') {
   process.argv.shift();
   program = path.basename(process.argv[1], '.js');
 }
 
+debug(`Decoded program ${program}`)
+
 const argv = process.argv.slice(2);
 const parser = new InputParser();
 
+debug('Parsing arguments...')
+
 parser.parse(argv).then((input) => {
+  debug('Parsing arguments... done.')
+  debug(`Dispatching ${input.length} input argument(s) to ${program}.`);
+
+  verbose(`Got input %O from argv %O`, input, argv);
   const results = Dispatcher.execute(program, input);
 
   if (results.error) {
     console.log(results.error);
+    debug(`Error: %O`, results.error);
   } else {
+    debug(`Returning ${results.results.length} result(s) via stdout.`);
+    verbose(`%O`, results.results);
     /* TODO: May have to extract this out. */
     console.log(results.results.join('\n'));
   }
