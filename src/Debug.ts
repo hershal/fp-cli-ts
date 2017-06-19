c65bd7614271a934f3edb52341d1de234f8864c8
Hershal Bhave <hershal.bhave@gmail.com>
2017-06-18 21:15:22 -0500

    Appease the linter

--- src/Debug.ts
+++ src/Debug.ts
@@ -1,9 +1,9 @@
 import * as debugImport from 'debug';
 
 export default function Debug(aNamespace: string) {
-    const debugWrapper = function (...args: any[]): any {
+    const debugWrapper = function(...args: any[]): any {
         return debugImport(`fp-cli:${aNamespace}`).apply(null, args);
-    }
+    };
 
     return debugWrapper;
 }
