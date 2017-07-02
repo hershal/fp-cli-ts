9befbf7db5e705073666c65e40d9cf613d2da043
Hershal Bhave <hershal.bhave@gmail.com>
2017-07-02 15:16:16 -0700

    Extract stream handling from Dispatcher

--- src/Dispatcher.ts
+++ src/Dispatcher.ts
@@ -2,13 +2,15 @@ import Debug from './Debug';
 
 import * as SetOperations from './SetOperations';
 import * as TextOperations from './TextOperations';
-import { ISynchronousOperation, IStreamingOperation, IDispatcher, IStreamDelegate } from './Interfaces';
-import { StreamSerializer } from './StreamSerializer';
+import { ISynchronousOperation, IStreamingOperation,
+         IDispatcher, IStreamDelegate,
+         IStreamHandlerDelegate, IError } from './Interfaces';
+import { StreamHandler } from './StreamHandler';
 
 
 /* Synchronous Standard Input Dispatcher. */
 /* Synchronously processes stdin in its entirety. */
-class DispatcherStandardInputSync implements IDispatcher {
+export class DispatcherStandardInputSync implements IDispatcher {
     public outputStreamDelegate?: IStreamDelegate;
 
     constructor(delegate?: IStreamDelegate) {
@@ -39,54 +41,70 @@ class DispatcherStandardInputSync implements IDispatcher {
 /* If you don't provide a delegate, then the Dispatcher will accumulate output
  * in a buffer until stdin is closed and return the buffer in a Promise.
  * Otherwise, the Dispatcher publishes the output to the delegate. */
-class DispatcherStandardInputStream implements IDispatcher {
-    public outputStreamDelegate?: IStreamDelegate;
-    private serializer: StreamSerializer;
+export class DispatcherStandardInputStream implements IDispatcher, IStreamHandlerDelegate {
+    public outputStreamDelegate: IStreamDelegate;
+    private streamHandler: StreamHandler;
     private debug = Debug('DispatcherStandardInputStream');
 
-    /* This buffer is only used if we don't have a delegate. */
-    private buffer: string[];
+    private resolveCallback?: () => void;
+    private rejectCallback?: (error: IError) => void;
+    private operation?: IStreamingOperation;
+    private inputStream: any;
 
-    constructor(delegate?: IStreamDelegate) {
-        this.outputStreamDelegate = delegate;
-        this.serializer = new StreamSerializer();
-        this.buffer = [];
+    constructor(outputStreamDelegate?: IStreamDelegate, inputStream: any = process.stdin) {
+        this.outputStreamDelegate = outputStreamDelegate;
+        this.streamHandler = new StreamHandler();
+        this.streamHandler.delegate = this;
+        this.inputStream = inputStream;
     }
 
-    public dispatch(operation: IStreamingOperation, args: string[]): Promise<string[]> {
-        return new Promise((resolve, reject) => {
+    public dispatch(operation: IStreamingOperation, args: string[]): Promise<void> {
+        /* Error handling */
+        if (this.operation) {
+            return Promise.reject({
+                domain: 'API',
+                reason: 'There is currently an operation this dispatcher is ' +
+                    'handling. A dispatcher cannot dispatch multiple operations at a time.',
+                resolution: 'Please instantiate another dispatcher to dispatch ' +
+                    'another operation.'
+            });
+        }
+        if (this.rejectCallback || this.resolveCallback) {
+            return Promise.reject({
+                domain: 'Internal',
+                reason: 'There is a dangling Promise that this dispatcher is ' +
+                    'holding but has not fulfilled, even though the operation is ' +
+                    'not running. This is an internal error.',
+                resolution: `Please debug me. RejectCallback: ${this.rejectCallback} ` +
+                    `ResolveCallback: ${this.resolveCallback}.`
+            });
+        }
+
+        this.operation = operation;
+        return new Promise<void>((resolve, reject) => {
+            this.resolveCallback = resolve;
+            this.rejectCallback = reject;
             operation.parse(args);
-
-            const callback = (serialized: string): string => {
-                this.debug(`Stdin serializer callback called on string of length ${serialized.length}.`);
-                const processed = operation.run(serialized);
-                if (!this.outputStreamDelegate) {
-                    this.buffer.push(processed);
-                } else {
-                    this.buffer.forEach((l) => this.outputStreamDelegate.streamDidReceiveChunk(l));
-                    this.outputStreamDelegate.streamDidReceiveChunk(processed);
-                    this.buffer = [];
-                }
-                return processed;
-            };
-
-            process.stdin.on('data', (data: Buffer) => {
-                this.debug(`Stdin sent chunk of size ${data.length}.`);
-                this.serializer.serialize(data, (serialized) => callback(serialized));
-            });
-
-            process.stdin.on('end', () => {
-                this.debug(`Stdin ended.`);
-                this.serializer.flush((data) => callback(data));
-
-                if (this.outputStreamDelegate) {
-                    this.outputStreamDelegate.streamDidEnd();
-                }
-
-                resolve(this.buffer);
-                this.buffer = [];
-            });
+            this.streamHandler.once(this.inputStream);
         });
+    }
+
+    public streamChunkTriggerString() {
+        /* TODO: wire this to the operation... somehow. */
+        return '\n';
+    }
+
+    public streamDidReceiveChunk(chunk: string) {
+        const processed = this.operation.run(chunk);
+        this.outputStreamDelegate.streamDidReceiveChunk(processed);
+    }
+
+    public streamDidEnd() {
+        this.outputStreamDelegate.streamDidEnd();
+        this.resolveCallback();
+        this.resolveCallback = undefined;
+        this.rejectCallback = undefined;
+        this.operation = undefined;
     }
 }
 
@@ -115,8 +133,10 @@ export default class Dispatch {
         if (!operationSelector) {
             return Promise.reject({
                 domain: 'Internal',
-                reason: `Could not find ${program} in the set of supported operations.`,
-                resolution: 'Either add a new supported operation or change the desired operation to a supported one.',
+                reason: `Could not find ${program} in the set of supported` +
+                    ` operations.`,
+                resolution: 'Either add a new supported operation or change ' +
+                    'the desired operation to a supported one.',
             });
         }
 
